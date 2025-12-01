10a15432fe76f43080c99e8d4f4a740eaf16bb4a
unknown <altikoddigitalsolutions>
2025-12-01 17:09:03 +0300

    Add crypto.randomUUID polyfill

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Polyfill for crypto.randomUUID */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+                crypto.randomUUID = function() {
+                  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                  });
+                };
+              }
+            `,
+          }}
+        />
         <script
           src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
           async
