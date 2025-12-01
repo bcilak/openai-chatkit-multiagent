6f0fd5445e47dde079b557f09c30468cbc3d2db5
unknown <altikoddigitalsolutions>
2025-12-01 17:40:33 +0300

    Fix: Move crypto polyfill to head for early execution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,18 +26,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Polyfill for crypto.randomUUID */}
+        {/* Polyfill for crypto.randomUUID - MUST run before any other scripts */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
-                crypto.randomUUID = function() {
-                  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                    return v.toString(16);
-                  });
-                };
-              }
+              (function() {
+                if (typeof crypto === 'undefined') {
+                  window.crypto = {};
+                }
+                if (!crypto.randomUUID) {
+                  crypto.randomUUID = function() {
+                    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                      return v.toString(16);
+                    });
+                  };
+                }
+                if (!crypto.getRandomValues) {
+                  crypto.getRandomValues = function(array) {
+                    for (var i = 0; i < array.length; i++) {
+                      array[i] = Math.floor(Math.random() * 256);
+                    }
+                    return array;
+                  };
+                }
+              })();
             `,
           }}
         />
